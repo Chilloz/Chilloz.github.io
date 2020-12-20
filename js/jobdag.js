b23b7309e4cd4b2d7fa3181298a226e904bca88b
Chilloz <glennverlinden1@gmail.com>
2020-12-20 16:26:51 +0100

    update

--- js/jobdag.js
+++ js/jobdag.js
@@ -6,86 +6,61 @@ function startWedstrijd() {
     var y = document.getElementById("intro");
     x.style.display = "block";
     y.style.display = "none";
-    document.getElementById("audio").innerHTML += "<audio autoplay><source src='video/Let The Game Begin.ogg' type='audio/ogg'><source src='video/Let The Game Begin.mp3' type='audio/mpeg'></audio>"
 }
 
 function checkRondeSchaar(){
-
     document.getElementById("spelerhand").src = "img/scissors.png";
-    var keuzecomputer = "steen";
     var random = arrAfbeeldingen[Math.floor(Math.random()*4)];
     if (random == "img/rock.png"){
-        keuzecomputer = "steen";
         document.getElementById("computerhand").src = "img/rock.png";
-    } else if (random == "img/paper.png"){
-        keuzecomputer = "papier";
-        document.getElementById("computerhand").src = "img/paper.png";
-    } else if (random == "img/scissors.png"){
-        keuzecomputer = "schaar";
-        document.getElementById("computerhand").src = "img/scissors.png";
-    };
-    if (keuzecomputer == "steen"){
         computerscore++;
-        document.getElementById("puntencomputer").innerHTML = computerscore;
-    } else if (keuzecomputer == "papier"){
+        document.getElementById("puntencomputer").innerHTML = computerscore;      
+    } else if (random == "img/paper.png"){
+        document.getElementById("computerhand").src = "img/paper.png";
         spelerscore++;
         document.getElementById("puntenspeler").innerHTML = spelerscore;
-        if (spelerscore == 3){
-            location.replace("https://chilloz.github.io/main.html");
+    } else if (random == "img/scissors.png"){
+        document.getElementById("computerhand").src = "img/scissors.png";
     };
-    }
+    if (spelerscore == 3){
+            // location.replace("https://chilloz.github.io/main.html");
+    };
 }
 function checkRondeSteen(){
-
     document.getElementById("spelerhand").src = "img/rock.png";
-    var keuzecomputer = "steen";
     var random = arrAfbeeldingen[Math.floor(Math.random()*4)];
     if (random == "img/rock.png"){
-        keuzecomputer = "steen";
         document.getElementById("computerhand").src = "img/rock.png";
     } else if (random == "img/paper.png"){
-        keuzecomputer = "papier";
-        document.getElementById("computerhand").src = "img/paper.png";
-    } else if (random == "img/scissors.png"){
-        keuzecomputer = "schaar";
-        document.getElementById("computerhand").src = "img/scissors.png";
-    };
-    if (keuzecomputer == "papier"){
         computerscore++;
         document.getElementById("puntencomputer").innerHTML = computerscore;
-    } else if (keuzecomputer == "schaar"){
+        document.getElementById("computerhand").src = "img/paper.png";
+    } else if (random == "img/scissors.png"){
         spelerscore++;
         document.getElementById("puntenspeler").innerHTML = spelerscore;
-        if (spelerscore == 3){
-            location.replace("https://chilloz.github.io/main.html");
+        document.getElementById("computerhand").src = "img/scissors.png";
     };
-    }
+    if (spelerscore == 3){
+            // location.replace("https://chilloz.github.io/main.html");
+    };
 }
 function checkRondePapier(){
-
     document.getElementById("spelerhand").src = "img/paper.png";
-    var keuzecomputer = "steen";
     var random = arrAfbeeldingen[Math.floor(Math.random()*4)];
     if (random == "img/rock.png"){
-        keuzecomputer = "steen";
-        document.getElementById("computerhand").src = "img/rock.png";
-    } else if (random == "img/paper.png"){
-        keuzecomputer = "papier";
-        document.getElementById("computerhand").src = "img/paper.png";
-    } else if (random == "img/scissors.png"){
-        keuzecomputer = "schaar";
-        document.getElementById("computerhand").src = "img/scissors.png";
-    };
-    if (keuzecomputer == "schaar"){
-        computerscore++;
-        document.getElementById("puntencomputer").innerHTML = computerscore;
-    } else if (keuzecomputer == "steen"){
         spelerscore++;
         document.getElementById("puntenspeler").innerHTML = spelerscore;
-        if (spelerscore == 3){
-            location.replace("https://chilloz.github.io/main.html");
+        document.getElementById("computerhand").src = "img/rock.png";
+    } else if (random == "img/paper.png"){
+        document.getElementById("computerhand").src = "img/paper.png";
+    } else if (random == "img/scissors.png"){
+        computerscore++;
+        document.getElementById("puntencomputer").innerHTML = computerscore;
+        document.getElementById("computerhand").src = "img/scissors.png";
     };
-    }
+    if (spelerscore == 3){
+            // location.replace("https://chilloz.github.io/main.html");
+    };
 }
 
 
